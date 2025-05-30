032734e5ac4508baddc08592751c5c7e49146919
AnastasiiaRachynska <rachynska.anastasiia@lll.kpi.ua>
2025-05-30 23:14:15 +0300

    Додала отримання параметрів в MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,6 +11,15 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::GoToFood);
     connect(ui->pushButton_6, &QPushButton::clicked, this, &MainWindow::GoToDream);
     connect(ui->pushButton_5, &QPushButton::clicked, this, &MainWindow::GoToSport);
+    connect(ui->radioButton, &QRadioButton::toggled, this, [this](bool checked){
+        if (checked)
+            sex = 1;
+    });
+
+    connect(ui->radioButton_2, &QRadioButton::toggled, this, [this](bool checked){
+        if (checked)
+            sex = 2;
+    });
 }
 
 MainWindow::~MainWindow()
@@ -20,6 +30,25 @@ MainWindow::~MainWindow()
 
 void MainWindow::GoToFood()
 {
+    bool check1, check2, check3;
+    QString heightText = ui->lineEdit->text().trimmed();
+    QString weightText = ui->lineEdit_2->text().trimmed();
+    QString ageText = ui->lineEdit_3->text().trimmed();
+    if(heightText.isEmpty() || weightText.isEmpty() || ageText.isEmpty())
+    {
+        QMessageBox::warning(this, "Помилка", "Усі поля мають бути заповненими");
+        return;
+    }
+
+    height = heightText.toInt(&check1);
+    weight = weightText.toInt(&check2);
+    age = ageText.toInt(&check3);
+    if(!check1 || !check2 || !check3)
+    {
+        QMessageBox::warning(this, "Помилка", "Введіть коректне число");
+        return;
+    }
+
     foodForm = new foodform(sex, height, weight, age, this);
     foodForm->show();
 }
